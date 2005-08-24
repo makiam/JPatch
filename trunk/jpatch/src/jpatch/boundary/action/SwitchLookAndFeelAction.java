e0f74c146817ec3030d111e5e8f370c93d484f87
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2005-08-24 13:51:15 +0000

    *** empty log message ***

--- trunk/jpatch/src/jpatch/boundary/action/SwitchLookAndFeelAction.java
+++ trunk/jpatch/src/jpatch/boundary/action/SwitchLookAndFeelAction.java
@@ -22,10 +22,6 @@ public class SwitchLookAndFeelAction extends AbstractAction {
 	public void actionPerformed(ActionEvent actionEvent) {
 		try {
 			JPatchSettings.getInstance().strPlafClassName = lookAndFeel.getClass().getName();
-			if (lookAndFeel.getClass().getName().endsWith("MetalLookAndFeel"))
-				MetalLookAndFeel.setCurrentTheme(new OceanTheme());
-			else if (lookAndFeel.getClass().getName().endsWith("SmoothLookAndFeel"))
-				SmoothLookAndFeel.setCurrentTheme(new SmoothTheme());
 			UIManager.setLookAndFeel(lookAndFeel);
 			SwingUtilities.updateComponentTreeUI(MainFrame.getInstance());
 		} catch (Exception e) {
