8bac01c595a84bfd2b847d338698c130b229e690
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2005-09-05 14:34:18 +0000

    *** empty log message ***

--- jpatch/src/jpatch/control/edit/CompoundRemoveHookCurve.java
+++ jpatch/src/jpatch/control/edit/CompoundRemoveHookCurve.java
@@ -5,37 +5,46 @@ import jpatch.entity.*;
 public class CompoundRemoveHookCurve extends JPatchCompoundEdit {
 	
 	public CompoundRemoveHookCurve(Curve curve) {
-		addEdit(new AtomicRemoveCurve(curve));
+		if (curve.getStart().getParentHook() == null)
+			throw new IllegalArgumentException("Curve " + curve + " is not a hook curve");
 		ControlPoint[] acp = curve.getControlPointArray();
-		if (acp != null) {
-			for (int n = 0; n < acp.length; n++) {
-				
-				if (acp[n].getNextAttached() != null && acp[n].getNextAttached().isHook()) {
-					Curve hookCurve = acp[n].getNextAttached().getCurve();
-//					addEdit(new RemoveControlPointFromSelectionsEdit(acp[n].getNextAttached()));
-					addEdit(new AtomicRemoveControlPointFromCurve(acp[n].getNextAttached()));
-					if (hookCurve.getLength() == 2) {
-						addEdit(new AtomicChangeControlPoint.ChildHook(hookCurve.getStart().getParentHook(),null));
-						addEdit(new RemoveControlPointFromSelectionsEdit(hookCurve.getStart()));
-						addEdit(new RemoveControlPointFromSelectionsEdit(hookCurve.getStart().getEnd()));
-						addEdit(new AtomicRemoveCurve(hookCurve));
-					}
-				}
-				//System.out.println("rem curve pt " + acp[n].number());
-				addEdit(new AtomicDetatchControlPoint(acp[n]));
-				addEdit(new AtomicChangeControlPoint.Curve(acp[n],null));
-				//System.out.println(acp[n].getCurve());
-				//ControlPoint[] acpStack = acp[n].getStack();
-				
-				if (acp[n].getHookPos() > 0 && acp[n].getHookPos() < 1) {
-					//System.out.println("x " + acp[n] + " " + acp[n].getPrevAttached());
-					if (acp[n].getPrevAttached() != null) {
-						addEdit(new CompoundDeleteControlPoint(acp[n].getPrevAttached()));
-					}
-				}
-				//addEdit(new DeleteControlPointEdit(acp[n]));
-//				addEdit(new RemoveControlPointFromSelectionsEdit(acp[n]));
-			}
+		for (int i = 0; i < acp.length; i++) {
+			addEdit(new CompoundDeleteControlPoint(acp[i]));
 		}
+		addEdit(new AtomicRemoveCurve(curve));
 	}
 }
+
+//		addEdit(new AtomicRemoveCurve(curve));
+//		ControlPoint[] acp = curve.getControlPointArray();
+//		if (acp != null) {
+//			for (int n = 0; n < acp.length; n++) {
+//				
+//				if (acp[n].getNextAttached() != null && acp[n].getNextAttached().isHook()) {
+//					Curve hookCurve = acp[n].getNextAttached().getCurve();
+////					addEdit(new RemoveControlPointFromSelectionsEdit(acp[n].getNextAttached()));
+//					addEdit(new AtomicRemoveControlPointFromCurve(acp[n].getNextAttached()));
+//					if (hookCurve.getLength() == 2) {
+//						addEdit(new AtomicChangeControlPoint.ChildHook(hookCurve.getStart().getParentHook(),null));
+//						addEdit(new RemoveControlPointFromSelectionsEdit(hookCurve.getStart()));
+//						addEdit(new RemoveControlPointFromSelectionsEdit(hookCurve.getStart().getEnd()));
+//						addEdit(new AtomicRemoveCurve(hookCurve));
+//					}
+//				}
+//				//System.out.println("rem curve pt " + acp[n].number());
+//				addEdit(new AtomicDetatchControlPoint(acp[n]));
+//				addEdit(new AtomicChangeControlPoint.Curve(acp[n],null));
+//				//System.out.println(acp[n].getCurve());
+//				//ControlPoint[] acpStack = acp[n].getStack();
+//				
+//				if (acp[n].getHookPos() > 0 && acp[n].getHookPos() < 1) {
+//					//System.out.println("x " + acp[n] + " " + acp[n].getPrevAttached());
+//					if (acp[n].getPrevAttached() != null) {
+//						addEdit(new CompoundDeleteControlPoint(acp[n].getPrevAttached()));
+//					}
+//				}
+//				//addEdit(new DeleteControlPointEdit(acp[n]));
+////				addEdit(new RemoveControlPointFromSelectionsEdit(acp[n]));
+//			}
+//		}
+
