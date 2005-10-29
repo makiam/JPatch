bc564fea9f31a6436f97e885e4ef57f72e973320
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2005-10-29 17:57:12 +0000

    *** empty log message ***

--- trunk/jpatch/src/jpatch/boundary/action/MakeFivePointPatchAction.java
+++ trunk/jpatch/src/jpatch/boundary/action/MakeFivePointPatchAction.java
@@ -22,168 +22,169 @@ public final class MakeFivePointPatchAction extends AbstractAction {
 		Selection selection = MainFrame.getInstance().getSelection();
 		Model model = MainFrame.getInstance().getModel();
 		ArrayList points = new ArrayList();
-		if (selection != null) {
-			for (Iterator it = model.getCurveSet().iterator(); it.hasNext(); ) {
-				for(ControlPoint cp = (ControlPoint) it.next(); cp != null; cp = cp.getNextCheckNextLoop()) {
-					if (selection.contains(cp))
-						points.add(cp);
-					else if (cp.getParentHook() != null && selection.contains(cp.getParentHook().getHead())) {
-						points.add(cp);
+		if (selection == null)
+			return;
+		
+		for (Iterator it = model.getCurveSet().iterator(); it.hasNext(); ) {
+			for(ControlPoint cp = (ControlPoint) it.next(); cp != null; cp = cp.getNextCheckNextLoop()) {
+				if (selection.contains(cp))
+					points.add(cp);
+				else if (cp.getParentHook() != null && selection.contains(cp.getParentHook().getHead())) {
+					points.add(cp);
 				}
 			}
-                	
-			//test:
-			
-				ControlPoint[] acp = new ControlPoint[points.size()];
-				points.toArray(acp);
-				HashMap mapHeadList = new HashMap();
-				HashSet setHeads = new HashSet();
-				for (int i = 0; i < acp.length; i++) {
-					ControlPoint head = acp[i].trueHead();
-					setHeads.add(head);
-					if (acp[i].getParentHook() != null) {
-						ArrayList neighbors = (ArrayList) mapHeadList.get(head);
-						if (neighbors == null) {
-							neighbors = new ArrayList();
-							mapHeadList.put(head,neighbors);
-						}
-						if (acp[i].getNext() != null) {
-							neighbors.add(acp[i]);
-							neighbors.add(acp[i].getNext());
-						}
-						if (acp[i].getPrev() != null) {
-							neighbors.add(acp[i]);
-							neighbors.add(acp[i].getPrev());
-						}
+		}
+		//test:
+		
+		ControlPoint[] acp = new ControlPoint[points.size()];
+		points.toArray(acp);
+		HashMap mapHeadList = new HashMap();
+		HashSet setHeads = new HashSet();
+		for (int i = 0; i < acp.length; i++) {
+			ControlPoint head = acp[i].trueHead();
+			setHeads.add(head);
+			if (acp[i].getParentHook() != null) {
+				ArrayList neighbors = (ArrayList) mapHeadList.get(head);
+				if (neighbors == null) {
+					neighbors = new ArrayList();
+					mapHeadList.put(head,neighbors);
+				}
+				if (acp[i].getNext() != null) {
+					neighbors.add(acp[i]);
+					neighbors.add(acp[i].getNext());
+				}
+				if (acp[i].getPrev() != null) {
+					neighbors.add(acp[i]);
+					neighbors.add(acp[i].getPrev());
+				}
+			}
+		}
+		
+		//System.out.println(setHeads);
+		//
+		//for (Curve curve = model.getFirstCurve(); curve != null; curve = curve.getNext()) {
+		//	for (ControlPoint cp = curve.getStart(); cp != null; cp = cp.getNextCheckNextLoop()) {
+		//		if (cp.getParentHook() != null && setHeads.contains(cp.getParentHook().getHead())) {
+		//			ControlPoint head = cp.getParentHook().getHead();
+		//			//setHeads.add(head);
+		//			
+		//			ArrayList neighbors = (ArrayList) mapHeadList.get(head);
+		//			if (neighbors == null) {
+		//				neighbors = new ArrayList();
+		//				mapHeadList.put(head,neighbors);
+		//			}
+		//			if (cp.getNext() != null) {
+		//				neighbors.add(cp);
+		//				neighbors.add(cp.getNext());
+		//			}
+		//			if (cp.getPrev() != null) {
+		//				neighbors.add(cp);
+		//				neighbors.add(cp.getPrev());
+		//			}
+		//			
+		//	      	}
+		//	}
+		//}
+		
+		
+		for (int i = 0; i < acp.length; i++) {
+			ControlPoint head = acp[i].trueHead();
+			if (acp[i].getParentHook() == null) {
+				ArrayList neighbors = (ArrayList) mapHeadList.get(head);
+				if (neighbors == null) {
+					neighbors = new ArrayList();
+					mapHeadList.put(head,neighbors);
+				}
+				ControlPoint[] stack = acp[i].getStack();
+				for (int j = 0; j < stack.length; j++) {
+					if (stack[j].getNext() != null) {
+						neighbors.add(stack[j]);
+						neighbors.add(stack[j].getNext());
+					}
+					if (stack[j].getPrev() != null) {
+						neighbors.add(stack[j]);
+						neighbors.add(stack[j].getPrev());
 					}
 				}
-				
-				//System.out.println(setHeads);
-				//
-				//for (Curve curve = model.getFirstCurve(); curve != null; curve = curve.getNext()) {
-				//	for (ControlPoint cp = curve.getStart(); cp != null; cp = cp.getNextCheckNextLoop()) {
-				//		if (cp.getParentHook() != null && setHeads.contains(cp.getParentHook().getHead())) {
-				//			ControlPoint head = cp.getParentHook().getHead();
-				//			//setHeads.add(head);
-				//			
-				//			ArrayList neighbors = (ArrayList) mapHeadList.get(head);
-				//			if (neighbors == null) {
-				//				neighbors = new ArrayList();
-				//				mapHeadList.put(head,neighbors);
-				//			}
-				//			if (cp.getNext() != null) {
-				//				neighbors.add(cp);
-				//				neighbors.add(cp.getNext());
-				//			}
-				//			if (cp.getPrev() != null) {
-				//				neighbors.add(cp);
-				//				neighbors.add(cp.getPrev());
-				//			}
-				//			
-				//	      	}
-				//	}
-				//}
-							
-							
-				for (int i = 0; i < acp.length; i++) {
-					ControlPoint head = acp[i].trueHead();
-					if (acp[i].getParentHook() == null) {
-						ArrayList neighbors = (ArrayList) mapHeadList.get(head);
-						if (neighbors == null) {
-							neighbors = new ArrayList();
-							mapHeadList.put(head,neighbors);
-						}
-						ControlPoint[] stack = acp[i].getStack();
-						for (int j = 0; j < stack.length; j++) {
-							if (stack[j].getNext() != null) {
-								neighbors.add(stack[j]);
-								neighbors.add(stack[j].getNext());
-							}
-							if (stack[j].getPrev() != null) {
-								neighbors.add(stack[j]);
-								neighbors.add(stack[j].getPrev());
-							}
+			}
+		}
+		//for (Iterator it = setHeads.iterator(); it.hasNext(); ) {
+		//	ControlPoint head = (ControlPoint) it.next();
+		//	//System.out.println("HEAD " + head.number());
+		//	ArrayList neighbors = (ArrayList) mapHeadList.get(head);
+		//	for (Iterator it2 = neighbors.iterator(); it2.hasNext(); ) {
+		//		System.out.println("\t" + ((ControlPoint) it2.next()).number());
+		//	}
+		//}
+		if (setHeads.size() == 5) {
+			//System.out.println();
+			ArrayList fivePointPatch = new ArrayList();
+			ArrayList toGoList = new ArrayList(setHeads);
+			ControlPoint neighborA;
+			ControlPoint neighborB;
+			ControlPoint currentHead = (ControlPoint) toGoList.get(0);
+			ControlPoint first = currentHead;
+			toGoList.remove(currentHead);
+			for (int i = 0; i < 5; i++) {
+				//System.out.println(currentHead.number());
+				//System.out.println(toGoList);
+				ArrayList neighborList = (ArrayList) mapHeadList.get(currentHead);
+				//System.out.println("neighborlist:" + neighborList);
+				Iterator neighborIterator = neighborList.iterator();
+				boolean found = false;
+				loop:
+					while (neighborIterator.hasNext()) {
+						neighborA = (ControlPoint) neighborIterator.next();
+						neighborB = (ControlPoint) neighborIterator.next();
+						//System.out.println(neighborA.number() + " " + neighborA.trueHead().number() + " " + neighborB.number() + " " + neighborB.trueHead().number());
+						if (toGoList.contains(neighborB.trueHead()) || (toGoList.size() == 0 && neighborB.trueHead() == first)) {
+							fivePointPatch.add(neighborA);
+							fivePointPatch.add(neighborB);
+							currentHead = neighborB.trueHead();
+							toGoList.remove(currentHead);
+							found = true;
+							break loop;
 						}
 					}
+				if (!found) {
+					//System.out.println("*break*");
+					return;
 				}
-				//for (Iterator it = setHeads.iterator(); it.hasNext(); ) {
-				//	ControlPoint head = (ControlPoint) it.next();
-				//	//System.out.println("HEAD " + head.number());
-				//	ArrayList neighbors = (ArrayList) mapHeadList.get(head);
-				//	for (Iterator it2 = neighbors.iterator(); it2.hasNext(); ) {
-				//		System.out.println("\t" + ((ControlPoint) it2.next()).number());
-				//	}
-				//}
-				if (setHeads.size() == 5) {
-					//System.out.println();
-					ArrayList fivePointPatch = new ArrayList();
-					ArrayList toGoList = new ArrayList(setHeads);
-					ControlPoint neighborA;
-					ControlPoint neighborB;
-					ControlPoint currentHead = (ControlPoint) toGoList.get(0);
-					ControlPoint first = currentHead;
-					toGoList.remove(currentHead);
-					for (int i = 0; i < 5; i++) {
-						//System.out.println(currentHead.number());
-						//System.out.println(toGoList);
-						ArrayList neighborList = (ArrayList) mapHeadList.get(currentHead);
-						//System.out.println("neighborlist:" + neighborList);
-						Iterator neighborIterator = neighborList.iterator();
-						boolean found = false;
-						loop:
-						while (neighborIterator.hasNext()) {
-							neighborA = (ControlPoint) neighborIterator.next();
-							neighborB = (ControlPoint) neighborIterator.next();
-							//System.out.println(neighborA.number() + " " + neighborA.trueHead().number() + " " + neighborB.number() + " " + neighborB.trueHead().number());
-							if (toGoList.contains(neighborB.trueHead()) || (toGoList.size() == 0 && neighborB.trueHead() == first)) {
-								fivePointPatch.add(neighborA);
-								fivePointPatch.add(neighborB);
-								currentHead = neighborB.trueHead();
-								toGoList.remove(currentHead);
-								found = true;
-								break loop;
-							}
-						}
-						if (!found) {
-							//System.out.println("*break*");
-							return;
-						}
-					}
-					//for (Iterator it = fivePointPatch.iterator(); it.hasNext(); ) {
-					//	System.out.println(((ControlPoint) it.next()).number());
-					//}
-					if (fivePointPatch.size() == 10) {
-						acp = new ControlPoint[10];
-						for (int i = 0; i < 10; i++) {
-							acp[i] = (ControlPoint) fivePointPatch.get(i);
-						}
-						if (
-							acp[1].trueCp() != acp[2].trueCp() &&
-							acp[3].trueCp() != acp[4].trueCp() &&
-							acp[5].trueCp() != acp[6].trueCp() &&
-							acp[7].trueCp() != acp[8].trueCp() &&
-							acp[9].trueCp() != acp[0].trueCp()
-						) {
-							Patch patch = new Patch(acp);
-							ControlPoint[] acp5 = new ControlPoint[] {
-									acp[0].trueHead(),
-									acp[2].trueHead(),
-									acp[4].trueHead(),
-									acp[6].trueHead(),
-									acp[8].trueHead()
-									//acp[0].getHead(),
-									//acp[2].getHead(),
-									//acp[4].getHead(),
-									//acp[6].getHead(),
-									//acp[8].getHead()
-								};
-							model.getCandidateFivePointPatchList().add(acp5);
-							MainFrame.getInstance().getUndoManager().addEdit(new AtomicAddPatch(patch));
-							MainFrame.getInstance().getJPatchScreen().update_all();
-						}
-					}
+			}
+			//for (Iterator it = fivePointPatch.iterator(); it.hasNext(); ) {
+			//	System.out.println(((ControlPoint) it.next()).number());
+			//}
+			if (fivePointPatch.size() == 10) {
+				acp = new ControlPoint[10];
+				for (int i = 0; i < 10; i++) {
+					acp[i] = (ControlPoint) fivePointPatch.get(i);
+				}
+				if (
+						acp[1].trueCp() != acp[2].trueCp() &&
+						acp[3].trueCp() != acp[4].trueCp() &&
+						acp[5].trueCp() != acp[6].trueCp() &&
+						acp[7].trueCp() != acp[8].trueCp() &&
+						acp[9].trueCp() != acp[0].trueCp()
+				) {
+					Patch patch = new Patch(acp);
+					ControlPoint[] acp5 = new ControlPoint[] {
+							acp[0].trueHead(),
+							acp[2].trueHead(),
+							acp[4].trueHead(),
+							acp[6].trueHead(),
+							acp[8].trueHead()
+							//acp[0].getHead(),
+							//acp[2].getHead(),
+							//acp[4].getHead(),
+							//acp[6].getHead(),
+							//acp[8].getHead()
+					};
+					model.getCandidateFivePointPatchList().add(acp5);
+					MainFrame.getInstance().getUndoManager().addEdit(new AtomicAddPatch(patch));
+					MainFrame.getInstance().getJPatchScreen().update_all();
 				}
 			}
 		}
 	}
 }
+
