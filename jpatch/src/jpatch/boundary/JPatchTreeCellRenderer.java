920a172b3a23044db7eb7d02a42954b19fe0f2e0
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2006-01-10 15:46:50 +0000

    *** empty log message ***

--- jpatch/src/jpatch/boundary/JPatchTreeCellRenderer.java
+++ jpatch/src/jpatch/boundary/JPatchTreeCellRenderer.java
@@ -2,6 +2,10 @@ package jpatch.boundary;
 
 import javax.swing.tree.*;
 import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
+
 import javax.swing.*;
 
 import jpatch.entity.*;
@@ -12,37 +16,35 @@ public class JPatchTreeCellRenderer extends DefaultTreeCellRenderer {
 	 */
 	private static final long serialVersionUID = 1L;
 //	private ImageIcon iconRoot = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/root.png"));
-	private ImageIcon iconModel = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/model.png"));
-	private ImageIcon iconMaterials = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/materials.png"));
-	private ImageIcon iconExpressions = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/morphs.png"));
-	private ImageIcon iconBones = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/bones.png"));
+	private static ImageIcon iconModel = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/model.png"));
+	private static ImageIcon iconMaterials = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/materials.png"));
+	private static ImageIcon iconExpressions = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/morphs.png"));
+	private static ImageIcon iconBones = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/bones.png"));
 //	private ImageIcon iconGroup = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/group.png"));
-	private ImageIcon iconBone = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/bone.png"));
-	private ImageIcon iconRdofRed1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_1.png"));
-	private ImageIcon iconRdofRed2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_2.png"));
-	private ImageIcon iconRdofRed3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_3.png"));
-	private ImageIcon iconRdofGreen1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_1.png"));
-	private ImageIcon iconRdofGreen2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_2.png"));
-	private ImageIcon iconRdofGreen3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_3.png"));
-	private ImageIcon iconRdofBlue1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_1.png"));
-	private ImageIcon iconRdofBlue2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_2.png"));
-	private ImageIcon iconRdofBlue3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_3.png"));
+	private static ImageIcon iconBone = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/bone.png"));
+	private static ImageIcon iconRdofRed1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_1.png"));
+	private static ImageIcon iconRdofRed2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_2.png"));
+	private static ImageIcon iconRdofRed3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_red_3.png"));
+	private static ImageIcon iconRdofGreen1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_1.png"));
+	private static ImageIcon iconRdofGreen2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_2.png"));
+	private static ImageIcon iconRdofGreen3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_green_3.png"));
+	private static ImageIcon iconRdofBlue1 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_1.png"));
+	private static ImageIcon iconRdofBlue2 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_2.png"));
+	private static ImageIcon iconRdofBlue3 = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/rdof_blue_3.png"));
 //	private ImageIcon iconTransDof = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/trans_dof.png"));
 //	private ImageIcon iconMuscle = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/muscle.png"));
-	private ImageIcon iconMaterial = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/material.png"));
-	private ImageIcon iconMorph = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/morph.png"));
-	private ImageIcon iconSelections = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/selections.png"));
-	private ImageIcon iconSelection = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/selection.png"));
-	private ImageIcon iconTarget = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/target.png"));
+	private static ImageIcon iconMaterial = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/material.png"));
+	private static ImageIcon iconMorph = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/morph.png"));
+	private static ImageIcon iconSelections = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/selections.png"));
+	private static ImageIcon iconSelection = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/selection.png"));
+	private static ImageIcon iconTarget = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/target.png"));
 	
-	private ImageIcon iconAnimation = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/animation.png"));
-	private ImageIcon iconModels = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/models.png"));
-	private ImageIcon iconCameras = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/camera.png"));
-	private ImageIcon iconLights = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/light.png"));
-	private ImageIcon iconCamera = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/camera.png"));
-	private ImageIcon iconLight = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/light.png"));
-	
-	
+	private static ImageIcon iconAnimation = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/animation.png"));
+	private static ImageIcon iconModels = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/models.png"));
+	private static ImageIcon iconCameras = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/camera.png"));
+	private static ImageIcon iconLights = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/light.png"));
+	private static ImageIcon iconCamera = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/camera.png"));
+	private static ImageIcon iconLight = new ImageIcon(ClassLoader.getSystemResource("jpatch/images/tree/light.png"));
 	
 	public Component getTreeCellRendererComponent(JTree tree,Object value,boolean sel,boolean expanded,boolean leaf,int row,boolean hasFocus) {
 		super.getTreeCellRendererComponent(tree,value,sel,expanded,leaf,row,hasFocus);
@@ -95,7 +97,8 @@ public class JPatchTreeCellRenderer extends DefaultTreeCellRenderer {
 						setIcon(iconRdofBlue3);
 				}
 			} else {
-				setIcon(iconMorph);
+//				setIcon(iconMorph);
+				return new MorphSlider(tree,value,sel,expanded,leaf,row,hasFocus);
 			}
 		}
 		else if (value instanceof MorphTarget) {
@@ -112,6 +115,50 @@ public class JPatchTreeCellRenderer extends DefaultTreeCellRenderer {
 		else if (value instanceof AnimModel)
 			setIcon(iconModel);
 		return this;
+	}
+	
+	public static class MorphSlider extends DefaultTreeCellRenderer {
+		private Morph morph;
+		private JTree tree;
+		public MorphSlider(JTree tree,Object value,boolean sel,boolean expanded,boolean leaf,int row,boolean hasFocus) {
+			super.getTreeCellRendererComponent(tree,value,sel,expanded,leaf,row,hasFocus);
+			this.morph = (Morph) value;
+			this.tree = tree;
+			setIcon(iconMorph);
+			Dimension dim = getPreferredSize();
+			if (dim.width < 120)
+				dim.width = 120;
+			System.out.println(dim);
+			setMinimumSize(dim);
+			setPreferredSize(dim);
+//			setMaximumSize(dim);
+		}
+		
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.24f));
+			float w = morph.getSliderValue();
+			float h = w / 10;
+			float left = 20;
+			float right = left + 100;
+			float bottom = getHeight() - 2;
+			GeneralPath path = new GeneralPath();
+			path.moveTo(left, bottom);
+			path.lineTo(left + w, bottom);
+			path.lineTo(left + w, bottom - h);
+			path.closePath();
+			((Graphics2D) g).fill(path);
+			g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.08f));
+			path = new GeneralPath();
+			path.moveTo(right, bottom);
+			path.lineTo(left + w, bottom);
+			path.lineTo(left + w, bottom - h);
+			path.lineTo(right, bottom - 10);
+			path.closePath();
+			((Graphics2D) g).fill(path);
+			g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.48f));
+			((Graphics2D) g).fill(new Rectangle2D.Float(left + w - 1, 1, 2, 14));
+		}
 	}
 }
 
