5d24da34d7f7902494ad66184fe00dab92f2a414
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2006-04-28 14:33:18 +0000

    *** empty log message ***

--- trunk/jpatch/src/jpatch/boundary/action/SetRotoscopeAction.java
+++ trunk/jpatch/src/jpatch/boundary/action/SetRotoscopeAction.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 
+import jpatch.auxilary.FileFilters;
 import jpatch.boundary.*;
-import jpatch.boundary.filefilters.*;
 import jpatch.boundary.settings.Settings;
 
 public final class SetRotoscopeAction extends AbstractAction {
@@ -21,7 +21,7 @@ public final class SetRotoscopeAction extends AbstractAction {
 	public void actionPerformed(ActionEvent actionEvent) {
 		ViewDefinition viewDef = MainFrame.getInstance().getJPatchScreen().getActiveViewport().getViewDefinition();
 		JFileChooser fileChooser = new JFileChooser(Settings.getInstance().directories.rotoscopeFiles);
-		fileChooser.addChoosableFileFilter(new ImageFilter());
+		fileChooser.addChoosableFileFilter(FileFilters.IMAGES);
 		if (fileChooser.showOpenDialog(MainFrame.getInstance()) == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			String filename = file.getPath();
