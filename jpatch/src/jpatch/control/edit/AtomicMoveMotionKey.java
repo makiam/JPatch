8422ed3b1e6739f6d3b16c06a677a3ef6611c33d
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2006-05-10 16:00:49 +0000

    *** empty log message ***

--- jpatch/src/jpatch/control/edit/AtomicMoveMotionKey.java
+++ jpatch/src/jpatch/control/edit/AtomicMoveMotionKey.java
@@ -7,13 +7,12 @@ public class AtomicMoveMotionKey extends JPatchAtomicEdit {
 	private MotionKey motionKey;
 	private float position;
 	
-	public AtomicMoveMotionKey(MotionCurve motionCurve, MotionKey motionKey, float position) {
+	public AtomicMoveMotionKey(MotionCurve motionCurve, MotionKey motionKey, float oldPosition) {
 		this.motionCurve = motionCurve;
 		this.motionKey = motionKey;
-		this.position = position;
+		this.position = oldPosition;
 		if (motionCurve.getKeyAt(motionKey.getPosition()) != motionKey)
 			throw new IllegalArgumentException("MotionKey " + motionKey + " is not on curve " + motionCurve);
-		swap();
 	}
 	
 	public void undo() {
