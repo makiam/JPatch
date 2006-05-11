a51f604c72a360b278d898e517d34c6a499a6a06
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2006-05-11 17:04:07 +0000

    *** empty log message ***

---
+++ trunk/jpatch/src/jpatch/boundary/timeline/Range.java
@@ -0,0 +1,8 @@
+package jpatch.boundary.timeline;
+
+public class Range {
+	public int firstTrack = Integer.MAX_VALUE;
+	public int lastTrack = Integer.MIN_VALUE;
+	public int firstFrame = Integer.MAX_VALUE;
+	public int lastFrame = Integer.MIN_VALUE;
+}
