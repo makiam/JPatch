79b1a6b739d1bef20e9e5a8999e560ef8d7f02dc
sascha_l <sascha_l@29e39ac5-f515-0410-9877-b01abe538bec>
2006-05-17 15:50:20 +0000

    *** empty log message ***

--- trunk/jpatch/src/jpatch/boundary/timeline/BoneTrack.java
+++ trunk/jpatch/src/jpatch/boundary/timeline/BoneTrack.java
@@ -208,7 +208,9 @@ public class BoneTrack extends Track {
 		return motionCurves;
 	}
 	
+	@Override
 	public MotionCurve getMotionCurve(MotionKey key) {
+		System.out.println("*");
 		if (key == null)
 			return null;
 		for (MotionCurve.Float motionCurve : motionCurves) {
