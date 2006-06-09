6afe946057b533ebf1890b18c8e6c1e2c3b6678d
sascha_l <sascha_l>
2006-06-09 16:15:06 +0000

    *** empty log message ***

--- jpatch/src/jpatch/boundary/timeline/AvarTrack.java
+++ jpatch/src/jpatch/boundary/timeline/AvarTrack.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AvarTrack.java,v 1.24 2006/06/07 20:07:44 sascha_l Exp $
+ * $Id: AvarTrack.java,v 1.25 2006/06/09 16:15:06 sascha_l Exp $
  *
  * Copyright (c) 2005 Sascha Ledinsky
  *
@@ -277,11 +277,11 @@ public class AvarTrack extends Track<MotionCurve.Float> {
 		if (!bExpanded)
 			return null;
 		
-		float min = motionCurve.getMin();
-		float max = motionCurve.getMax();
-		float scale = max - min;
-		int size = iExpandedHeight - 4;
-		int off = iExpandedHeight - 4 + Math.round(size * min / scale);
+//		float min = motionCurve.getMin();
+//		float max = motionCurve.getMax();
+//		float scale = max - min;
+//		int size = iExpandedHeight - 4;
+//		int off = iExpandedHeight - 4 + Math.round(size * min / scale);
 		
 		int frame = mx / timelineEditor.getFrameWidth() + (int) MainFrame.getInstance().getAnimation().getStart();
 		int hyIn = 0;
@@ -296,13 +296,13 @@ public class AvarTrack extends Track<MotionCurve.Float> {
 		key = motionCurve.getKeyAt(frame + TANGENT_LENGTH);
 		if (key != null) {
 			handleIn = getInTangent(key);
-			hyIn = off - Math.round(size / scale * handleIn.getValue());
+			hyIn = offset - Math.round(scale * handleIn.getValue());
 		}
 		
 		key = motionCurve.getKeyAt(frame - TANGENT_LENGTH);
 		if (key != null) {
 			handleOut = getOutTangent(key);
-			hyOut = off - Math.round(size / scale * handleOut.getValue());
+			hyOut = offset - Math.round(scale * handleOut.getValue());
 		}
 		
 		int dyIn = Math.abs(my - hyIn);
